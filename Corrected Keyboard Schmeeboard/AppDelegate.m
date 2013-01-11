ebfa45f7730c6775e7968ebd91946605b4fca967
Andrew Pontious <apontious@gmail.com>
2013-01-10 21:07:09 -0800

    Add "Corrected" version of project, which I will now change to have real solution.

---
+++ Corrected Keyboard Schmeeboard/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Corrected Keyboard Schmeeboard http://github.com/apontious/Keyboard-Schmeeboard/
+//
+//  Created by Andrew Pontious on 12/9/12.
+//  Copyright (c) 2012 Andrew Pontious. All rights reserved.
+//  Some right reserved: http://opensource.org/licenses/mit-license.php
+//
+
+#import "AppDelegate.h"
+
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
